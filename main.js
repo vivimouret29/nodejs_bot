234cb665811a9685ca95c78788406ca10b04f690
vivimouret29 <vivien.mouret@ynov.com>
2020-09-23 20:22:40 +0200

    ⚡️ improving some code

--- main.js
+++ main.js
@@ -1,53 +1,82 @@
 require('dotenv').config()
 
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 const TOKEN = process.env.DISCORD_TOKEN;
 
-//Toutes les actions à faire quand le bot se connecte
-bot.on("ready", function () {
-    console.log('daftbot vas tout détruire bip bip')
-})
+// variable personnelles
+const yesyes = new Discord.MessageEmbed()
+	.setTitle('YES YES YES YES YES')
+	.attachFiles(['./images/ElatedGlamorousAmericanindianhorse-size_restricted.gif'])
+const homonculus = new Discord.MessageEmbed()
+	.setTitle('je suis un petit être dans une fiole')
+	.attachFiles(['./images/received_3303082543102197.gif'])
 
-bot.login(TOKEN)
+// message de connexion au serveur
 
-bot.on('guildMemberAdd', member => {
+module.exports.run = async(client, message, args) => {
+	let User = args[0];
+	let Reason = args.slice(1).join(` `);
+}
+
+client.on("ready", function () {
+	console.log('bip boup.. ' + client.user.username + ' vas tout détruire !! bip bip..')
+	// console.log(client.users.cache.Collection[1])
+	// client.users.get('name', 'hbooex').id
+	// for (guild in client.guilds) {
+	// 	console.log('Nom du serveur : ' + guilds.name)
+	// 	for (member in guild.members) {
+	// 		console.log('Nom : ' + member.name)
+	// 		console.log('ID : ' + member.id)
+	// 	}
+	// }
+});
+
+// id des serveurs
+// '758064568911265834' => DevBot
+// '636204768842219531' => Games & Work
+// message de connexion d'un nouveau membre
+
+client.on('guildMemberAdd', member => {
   member.createDM().then(channel => {
-    return channel.send('Bienvenue à toi jeune padawan ' + member.displayName)
+	console.log('new member')
+	return channel.send('Bienvenue à toi jeune padawan ' + member.displayName)
   }).catch(console.error)
-})
+});
 
-//ajoute une variable msg pour remplacer message.content
-
-bot.on('message', message => { 
-	if (message.content == 'bot') {
-		message.reply('on sait tous que daftbot est supérieur à pybot')
-		console.log('reply bestbot')
+client.on('message', message => {
+	if (message.author === client.user) {
+		return
 	}
-	if (message.content === 'salope') {
-		message.reply('toi-même connard')
+	// switch (message)
+	if (message.content.includes('fuck')) {
+		message.channel.send('je sais mec....')
+		console.log('reply fuck')
+	}
+	if (message.content.includes('salope')) {
+		message.reply('tu devrais baisser d\'un ton')
 		console.log('reply salope')
 	}
-	if (message.content === 'pute') {
-                message.reply('pas les mamans')
+	if (message.content.includes('pute')) {
+                message.channel.send('roooh pas les mamans')
 		console.log('reply pute')
         }
-	if (message.content === 'crash') {
-                message.reply('je parie sur un ragequit')
+	if (message.content.includes('crash')) {
+                message.channel.send('je parie sur un ragequit')
 		console.log('reply crash')
         }
-	if (message.content == 'buy') {
-                message.reply('Remerciement de la part de toutes les équipes de Steam et Instant-Gaming')
+	if (message.content.includes('buy')) {
+                message.channel.send('Remerciement de la part de toutes les équipes de Steam et Instant-Gaming')
 		console.log('reply buy')
         }
-	//if (message.content === 'yes') {
-	//	message.content.channel.send('', {files: ["./images/ElatedGlamorousAmericanindianhorse-size_restricted.gif"]})
-	//	console.log('reply yes')
-	//}
-})
+	if (message.content.includes('yes')) {
+		message.channel.send(yesyes)
+		console.log('reply yes')
+	}
+	if (message.content.includes('daftbot')) {
+		message.channel.send(homonculus)
+		console.log('reply homonculus')
+	}
+});
 
-//Permissions du bot au sein du serveur
-
-//class discord.Permissions(permissions=515136) {
-//	discord.utils.oauth_url(757955750164430980,permissions=515136)
-//}
+client.login(TOKEN);
