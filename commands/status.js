cb19502b25f00d0ba36794fa8f8a5fb7e7324c63
vivimouret29 <vivien.mouret@ynov.com>
2020-10-01 17:51:29 +0200

    💩 bad code that will be reviewed in a hundred years

--- commands/status.js
+++ commands/status.js
@@ -1,34 +1,49 @@
-module.exports = {
+module.exports = {      // (╯°□°）╯︵ ┻━┻
     name: 'status',
     description: 'a dynamic status',
-    execute(message, args) {
-        const Discord = require('discord.js');
-        const client = new Discord.Client();
-
+    execute(message, args, client) {
         const stOnOff = args[0]
         const typeThings = args[1]
         const nameText = args[2]
         const urlLike = args[3]
 
         if (message.author.id === '431915542610313217') {
-            client
-                .on('ready', () => {
-                    client.user.setPresence({
-                        activity: {
-                            name: `${nameText}`,
-                            type: `${typeThings}`,
-                            url: `${urlLike}`
-                        },
-                        status: `${stOnOff}`
-                            .catch(
-                                console.error,
-                                message.channel.send(`problème avec ton status`)
-                            )
-                    });
-                })
-                .on('debug', console.log);
-            message.delete().catch(O_o => { })
-            message.channel.send('changement d\'activité !')
+            if (!urlLike === undefined) {
+                client
+                    .on('ready', () => {
+                        client.user.setPresence({
+                            activity: {
+                                name: `${nameText}`,
+                                type: `${typeThings}`,
+                                url: `${urlLike}`
+                            },
+                            status: `${stOnOff}`
+                                .catch(
+                                    console.error,
+                                    message.channel.send(`problème avec ton status`)
+                                )
+                        });
+                    })
+                message.delete().catch(O_o => { })
+                message.channel.send('changement d\'activité !')
+            } else {
+                client
+                    .on('ready', () => {
+                        client.user.setPresence({
+                            activity: {
+                                name: `${nameText}`,
+                                type: `${typeThings}`
+                            },
+                            status: `${stOnOff}`
+                                .catch(
+                                    console.error,
+                                    message.channel.send(`problème avec ton status`)
+                                )
+                        });
+                    })
+                message.delete().catch(O_o => { })
+                message.channel.send('changement d\'activité !')
+            }
         } else return message.channel.send('t\'as pas le droit d\'y toucher')
     }
 };
