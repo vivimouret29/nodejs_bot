b803500f85c9278c4ad68edf2fa4849a98386b59
vivimouret29 <vivien.mouret@ynov.com>
2020-10-01 21:06:13 +0200

    🎨 small redesign of the high code and addition of the console.log of the kick command

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 'use.strict'
 
-const Discord = require('discord.js');
-const ytdl = require('ytdl-core');
-const fs = require('fs');
-const client = new Discord.Client();
-const collection = new Discord.Collection();
-const date = new Date();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const replyFiles = fs.readdirSync('./reply').filter(file => file.endsWith('.js'));
 const {
 	prefix,
 	token
 } = require("./config.json");
 
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+
+const client = new Discord.Client();
+const collection = new Discord.Collection();
+const date = new Date();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const replyFiles = fs.readdirSync('./reply').filter(file => file.endsWith('.js'));
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
+
 	collection.set(command.name, command);
 }
 
 for (const file of replyFiles) {
 	const reply = require(`./reply/${file}`);
+
 	collection.set(reply.name, reply);
 }
 
@@ -27,7 +32,7 @@ client
 	.on('ready', () => {
 		client.user.setPresence({
 			activity: {
-				name: `réparer les pots cassés`,
+				name: `à réparer les pots`,
 				type: 'PLAYING',
 			},
 			status: 'idle'
@@ -38,20 +43,21 @@ client
 	.on('guildMemberAdd', member => {
 		member.createDM().then(channel => {
 			console.log(`[${replydate}] REPLY NEW MEMBER ${message.author.username}`)
-			return channel.send(`Jeune padawan ${member.displayName}, bienvenue à toi.`)
+			return channel.send(`Jeune padawan ${member.displayName}, bienvenue à toi.`);
 		})
 			.then(console.log)
 			.catch(console.error);
 	})
 	.on('message', async message => {
 
-		if (message.author.bot) return
+		if (message.author.bot) return;
 
-		const args = message.content.slice(prefix.length).trim().split(/ +/)
-		const command = args.shift().toLowerCase()
-		const msg = message.content.toLowerCase()
-		const autmsg = message.author.username
-		let replydate = `${date.getHours()}:${date.getMinutes()} - ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
+		const args = message.content.slice(prefix.length).trim().split(/ +/);
+		const command = args.shift().toLowerCase();
+		const msg = message.content.toLowerCase();
+		const autmsg = message.author.username;
+		const taggedUser = message.mentions.users.first();
+		let replydate = `${date.getHours()}:${date.getMinutes()} - ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
 
 		if (!message.content.startsWith(prefix)) {
 
@@ -73,7 +79,10 @@ client
 					console.log(`[${replydate}] REPLY ${command} FROM ${autmsg} WITH ${args} ERASED LINES`)
 				} else if (command === 'status') {
 					collection.get(command).execute(message, args, client);
-					console.log(`[${replydate}] REPLY ${command} ${args[0]} ${args[1]} ${args[2]} (${args[3]}), FROM ${autmsg}`)
+					console.log(`[${replydate}] REPLY ${command} ${args.join(" ")}, FROM ${autmsg}`)
+				} else if (command === 'kick') {
+					collection.get(command).execute(message, args, client);
+					console.log(`[${replydate}] REPLY ${command} FROM ${autmsg} TO ${taggedUser.username} lol`)
 				} else {
 					collection.get(command).execute(message, args, client);
 					console.log(`[${replydate}] REPLY ${command} FROM ${autmsg}`)
