b11ea2ae9a485d9e5177a8f6ddfba15716bb1ec1
vivimouret29 <vivien.mouret@ynov.com>
2020-10-03 17:21:35 +0200

    📝 add all functional changes

--- commands/help.js
+++ commands/help.js
@@ -5,11 +5,11 @@ module.exports = {
         message.delete().catch(O_o => { })
         message.channel.send('```Liste help :\n\
                 help : faire ce que tu fais ducon\n\
-                kick : kick + @ à tes risques et périls\n\
+                votekick : kick + @ à tes risques et périls\n\
                 ping : teste la latence\n\
                 prune : supprime des lignes (commande admin)\n\
                 say : me fait répéter n\'importe quelle connerie\n\
-                status : écris §status\n\
+                status : écris §status (commande admin actuellemnt)\n\
                                 - idle=inactif ; dnd=ne pas déranger ;\n\
                                 online=en ligne ; invisible=hors-ligne\n\
                                 - LISTENING, WATCHING, STREAMING, PLAYING\n\
