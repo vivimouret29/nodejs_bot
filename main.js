f93fa1b93e4509ba6e70d877052fa134c2863ebd
vivimouret29 <vivien.mouret@ynov.com>
2020-10-05 14:12:20 +0200

    :bug: fixing status functions

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const client = new Discord.Client();
 const collection = new Discord.Collection();
 const date = new Date();
 
-const replydate = `${date.getHours()}:${date.getMinutes()} - ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+const initdate = `${date.getHours()}:${date.getMinutes()} - ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const replyFiles = fs.readdirSync('./reply').filter(file => file.endsWith('.js'));
@@ -47,7 +47,7 @@ client
 	})
 	.on('guildMemberAdd', member => {
 		member.createDM().then(channel => {
-			console.log(`[${replydate}]# Games-&-Work/Homepage/general-chat/newMember/${member.user.username}`)
+			console.log(`[${getCurrentDatetime()}]# Games-&-Work/Homepage/general-chat/newMember/${member.user.username}`)
 			return channel.send(`Jeune padawan ${member.displayName}, bienvenue à toi.`);
 		})
 			.catch(console.error);
@@ -62,6 +62,11 @@ client
 		const authorMessage = message.author.username;
 		const member = message.guild.member(message.author);
 
+		function getCurrentDatetime() {
+			const newDate = new Date()
+			return `${newDate.getHours()}:${newDate.getMinutes()} - ${newDate.getDate()}/${newDate.getMonth()}/${newDate.getFullYear()}`
+		}
+
 		function uptimeFunction() {
 			let totalSeconds = (client.uptime / 1000);
 			let days = Math.floor(totalSeconds / 86400);
@@ -71,9 +76,10 @@ client
 			let minutes = Math.floor(totalSeconds / 60);
 			let seconds = Math.floor(totalSeconds % 60);
 
+			let update = initdate
 			message.delete().catch(O_o => { })
-			message.channel.send(`\`UPTIME|SATELLITE : ${days}D:${hours}H:${minutes}M:${seconds}S\``)
-			console.log(`[${replydate}]# ${authorMessage} :  ${msg}`);
+			message.channel.send(`\`UPDATE|SATELLITE : ${update}\`\n\`UPTIME|SATELLITE : ${days}D:${hours}H:${minutes}M:${seconds}S\``)
+			console.log(`[${getCurrentDatetime()}]# ${authorMessage} :  ${msg}`);
 		}
 
 		function statusFunction() {
@@ -112,7 +118,7 @@ client
 					message.channel.send('changement d\'activité !')
 				}
 			} else return message.channel.send('t\'as pas le droit d\'y toucher')
-			console.log(`[${replydate}]# ${authorMessage} :  ${msg}`);
+			console.log(`[${getCurrentDatetime()}]# ${authorMessage} :  ${msg}`);
 		}
 
 		async function kickCounter() {
@@ -132,10 +138,10 @@ client
 				await message.author.send(tkick)
 				await message.author.send('https://discord.gg/3ZQmHb')
 				await member.kick()
-				console.log(`[${replydate}]# ${authorMessage} :  ${prefix}${command} ${authorMessage}\n[${replydate}]# ${client.user.username} : lol`)
+				console.log(`[${getCurrentDatetime()}]# ${authorMessage} :  ${prefix}${command} ${authorMessage}\n[${getCurrentDatetime()}]# ${client.user.username} : lol`)
 			} else {
 				message.channel.send('fonctionnality not available for you yet xoxo')
-				console.log(`[${replydate}]# ${authorMessage} :  ${prefix}${command} ${authorMessage}`)
+				console.log(`[${getCurrentDatetime()}]# ${authorMessage} :  ${prefix}${command} ${authorMessage}`)
 			}
 		}
 
@@ -146,7 +152,7 @@ client
 
 			try {
 				collection.get(msg).execute(message);
-				console.log(`[${replydate}]# ${authorMessage} :  ${msg}`)
+				console.log(`[${getCurrentDatetime()}]# ${authorMessage} :  ${msg}`)
 			} catch (error) {
 				console.error(error);
 			}
@@ -163,13 +169,17 @@ client
 			try {
 				if (command === 'prune') {
 					collection.get(command).execute(message, args, client);
-					console.log(`[${replydate}]# ${authorMessage} :  ${msg}`)
+<<<<<<< Updated upstream
+					console.log(`[${getCurrentDatetime()}]# ${authorMessage} :  ${msg}`)
 					// } else if (command === 'kick') {
 					// 	collection.get(command).execute(message, args, client);
-					// 	console.log(`[${replydate}]# ${authorMessage} :  ${msg} \n[${replydate}]# ${client.user.username} : lol`)
+					// 	console.log(`[${getCurrentDatetime()}]# ${authorMessage} :  ${msg} \n[${getCurrentDatetime()}]# ${client.user.username} : lol`)
+=======
+					console.log(`[${replydate}]# ${authorMessage} :  ${msg}`)
+>>>>>>> Stashed changes
 				} else {
 					collection.get(command).execute(message, args, client);
-					console.log(`[${replydate}]# ${authorMessage} :  ${msg}`)
+					console.log(`[${getCurrentDatetime()}]# ${authorMessage} :  ${msg}`)
 				}
 			} catch (error) {
 				console.error(error);
