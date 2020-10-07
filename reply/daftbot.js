d588f71b795ff3263c8f646099798a5d7b84537a
vivimouret29 <vivien.mouret@ynov.com>
2020-10-07 16:20:58 +0200

    ⚡️ use of op link

--- reply/daftbot.js
+++ reply/daftbot.js
@@ -5,7 +5,7 @@ module.exports = {
         const Discord = require('discord.js');
         const botgif = new Discord.MessageEmbed()
             .setTitle('je suis un petit être dans une fiole')
-            .attachFiles(['./images/homonculus.gif']);
+            .attachFiles('https://i.skyrock.net/7297/93457297/pics/3273728730_1_2_yrHEcA4w.gif');
             
         message.channel.send(botgif)
     }
--- reply/yes.js
+++ reply/yes.js
@@ -5,7 +5,7 @@ module.exports = {
         const Discord = require('discord.js');
         const yesgif = new Discord.MessageEmbed()
             .setTitle('YES YES YES YES YES')
-            .attachFiles(['./images/YESYESYESYESYES.gif']);
+            .attachFiles('https://i.pinimg.com/originals/80/ca/e3/80cae3013cf2513e3cef9179f7d64073.gif');
 
         message.channel.send(yesgif)
     }
