e809df67b1563d5b5ede275e6d9ff1cdec0eb50f
Vivien Mouret <vivien.mouret@ynov.com>
2023-02-09 14:56:51 +0100

    ⚗️ integration of pepe diffuser

--- appdata/command.js
+++ appdata/command.js
@@ -2,7 +2,9 @@
 
 const package = require("../package.json"),
     { owner } = require("../config.json"),
-    { sendEmbed } = require('../embed.js');
+	fetch = require('node-fetch'),
+	fs = require('fs'),
+    { sendEmbed, randomColor } = require('../embed.js');
 
 module.exports = {
     invit: {
@@ -58,6 +60,61 @@ module.exports = {
         async execute(message, args) {
             var wait = await message.channel.send(args.language.pingWait);
             wait.edit(`Bip. ${args.language.pingEdit} ${wait.createdTimestamp - message.createdTimestamp}ms.. Bip Boup..`);
+        }
+    },
+    imagine: {
+        name: 'imagine',
+        description: 'a dynamic pepe',
+        args: true,
+        async execute(message, args) {
+            if (args.args.length == 0) { return sendEmbed(message, args.language.argsUndefined); };
+
+            var msg = await message.channel.send({
+                'channel_id': message.channel.channel_id,
+                'content': `${args.args.join(' ')}, *Waiting to display...*`
+            });
+        
+            const response = await fetch('https://dipl0-dipl0-pepe-diffuser.hf.space/run/predict', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    data: [
+                        args.args.join(' ')
+                    ]
+                })
+            });
+        
+            const data = await response.json(),
+                splitted = data.data[0].split(',')[1],
+                buffer = Buffer.from(splitted, 'base64');
+            let saveDiffuser = false;
+        
+            if (message.author.id == owner) { saveDiffuser = true; };
+        
+            fs.writeFileSync(`./styles/ai/${saveDiffuser ? args.args.join('') : 'pepe-diffuser'}.jpg`, buffer);
+            msg.edit({
+                'channel_id': message.channel.channel_id,
+                'content': '',
+                'tts': false,
+                'embeds': [{
+                    'type': 'rich',
+                    'title': 'Imagine',
+                    'description': `**${args.args.join(' ')}**\nDuration : ${data.duration} seconds\nAverage duration : ${data.average_duration} seconds\n\n[**Pepe Diffuser**](https://huggingface.co/Dipl0/pepe-diffuser)`,
+                    'color': randomColor(),
+                    'author': {
+                        'name': message.author.username,
+                        'icon_url': message.author.avatarURL({ format: 'png', dynamic: true, size: 1024 })
+                    },
+                    'footer': {
+                        'text': args.args.join(' '),
+                        'icon_url': 'https://aeiljuispo.cloudimg.io/v7/https://s3.amazonaws.com/moonup/production/uploads/1611668769240-noauth.png?w=200&h=200&f=face',
+                        'proxy_icon_url': 'https://huggingface.co/Dipl0/pepe-diffuser'
+                    }
+                }],
+                files: [
+                    `./styles/ai/${saveDiffuser ? args.args.join('') : 'pepe-diffuser'}.jpg`
+                ]
+            });
         }
     }
 };
--- embed.js
+++ embed.js
@@ -18,7 +18,7 @@ async function sendEmbed(message, content, descp) {
 				'name': message.author.username,
 				'icon_url': message.author.avatarURL({ format: 'png', dynamic: true, size: 1024 })
 			},
-			'footer': { 
+			'footer': {
 				'text': message.content,
 				'icon_url': 'https://cdn.discordapp.com/app-icons/757955750164430980/94a997258883caba5f553f98aea8df59.png?size=256',
 				'proxy_icon_url': 'https://discord.gg/ucwnMKKxZe'
@@ -26,16 +26,17 @@ async function sendEmbed(message, content, descp) {
 		}]
 	});
 
-	function randomColor() {
-		let chars = '0123456789abcdef'.split(''),
-			hex = '0x';
-
-		for (let i = 0; i < 6; i++) { hex += chars[Math.floor(Math.random() * 16)]; };
-
-		return Number(hex);
-	};
-
 	return messageToSend;
 };
 
+function randomColor() {
+	let chars = '0123456789abcdef'.split(''),
+		hex = '0x';
+
+	for (let i = 0; i < 6; i++) { hex += chars[Math.floor(Math.random() * 16)]; };
+
+	return Number(hex);
+};
+
 exports.sendEmbed = sendEmbed;
+exports.randomColor = randomColor;
