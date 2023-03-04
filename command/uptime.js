f17d1ab852fde56248dc281faee8e66d0b28a15a
Vivien Mouret <vivien.mouret@ynov.com>
2023-03-05 00:52:55 +0100

    💄 update ui uptime

--- command/uptime.js
+++ command/uptime.js
@@ -9,16 +9,23 @@ module.exports = {
     },
     async execute(message, client, language, args, initDateTime) {
         try {
-            let totalSeconds = (client.uptime / 1000);
-            let days = Math.floor(totalSeconds / 86400);
-            totalSeconds %= 86400;
-            let hours = Math.floor(totalSeconds / 3600);
-            totalSeconds %= 3600;
-            let minutes = Math.floor(totalSeconds / 60);
-            let seconds = Math.floor(totalSeconds % 60);
-            let start = initDateTime;
+            let totalSeconds = (client.uptime / 1000),
+                days,
+                hours,
+                minutes,
+                seconds,
+                start = initDateTime;
 
-            await sendEmbed(message, `${language.uptime} ${days}:${hours}:${minutes}:${seconds}\n${start}`)
+            Math.floor(totalSeconds / 86400) < 10 ? days = `0${Math.floor(totalSeconds / 86400)}` : days = Math.floor(totalSeconds / 86400);
+            totalSeconds %= 86400;
+
+            Math.floor(totalSeconds / 3600) < 10 ? hours = `0${Math.floor(totalSeconds / 3600)}` : hours = Math.floor(totalSeconds / 3600);
+            totalSeconds %= 3600;
+
+            Math.floor(totalSeconds / 60) < 10 ? minutes = `0${Math.floor(totalSeconds / 60)}` : minutes = Math.floor(totalSeconds / 60);
+            Math.floor(totalSeconds % 60) < 10 ? seconds = `0${Math.floor(totalSeconds % 60)}` : seconds = Math.floor(totalSeconds % 60);
+
+            await sendEmbed(message, `${language.uptime} **${days}:${hours}:${minutes}.${seconds}**\n${start}`)
                 .catch(err => {
                     message.reply({ 'content': language.error, 'ephemeral': true });
                     console.log(`[${getCurrentDatetime('comm')}] Error sending message SEERROR ${err}`);
--- slash/uptime.js
+++ slash/uptime.js
@@ -9,16 +9,26 @@ module.exports = {
         .setDescription('Pour connaître sa durée de fonctionnement'),
     async execute(message, client, language, initDateTime) {
         try {
-            let totalSeconds = (client.uptime / 1000);
-            let days = Math.floor(totalSeconds / 86400);
-            totalSeconds %= 86400;
-            let hours = Math.floor(totalSeconds / 3600);
-            totalSeconds %= 3600;
-            let minutes = Math.floor(totalSeconds / 60);
-            let seconds = Math.floor(totalSeconds % 60);
-            let start = initDateTime;
+            let totalSeconds = (client.uptime / 1000),
+                days,
+                hours,
+                minutes,
+                seconds,
+                start = initDateTime;
 
-            message.reply({ 'content': `${language.uptime} ${days}:${hours}:${minutes}:${seconds}\n${start}`, 'ephemeral': true });
+            Math.floor(totalSeconds / 86400) < 10 ? days = `0${Math.floor(totalSeconds / 86400)}` : days = Math.floor(totalSeconds / 86400);
+            totalSeconds %= 86400;
+
+            Math.floor(totalSeconds / 3600) < 10 ? hours = `0${Math.floor(totalSeconds / 3600)}` : hours = Math.floor(totalSeconds / 3600);
+            totalSeconds %= 3600;
+
+            Math.floor(totalSeconds / 60) < 10 ? minutes = `0${Math.floor(totalSeconds / 60)}` : minutes = Math.floor(totalSeconds / 60);
+            Math.floor(totalSeconds % 60) < 10 ? seconds = `0${Math.floor(totalSeconds % 60)}` : seconds = Math.floor(totalSeconds % 60);
+
+            message.reply({
+                'content': `${language.uptime} **${days}:${hours}:${minutes}.${seconds}**\n${start}`,
+                'ephemeral': true
+            });
         } catch (err) {
             message.reply({ 'content': language.error, 'ephemeral': true });
             console.log(`[${getCurrentDatetime('comm')}] Error function uptime() ${err}`);
