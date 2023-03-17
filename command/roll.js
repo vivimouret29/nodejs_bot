37382fac83807653b5ccd5094c7bc0d8143d09a7
Vivien Mouret <vivien.mouret@ynov.com>
2023-03-17 16:13:50 +0100

    💚 functionnal roll

--- command/roll.js
+++ command/roll.js
@@ -1,7 +1,12 @@
 'use.strict'
 
-const { randomIntFromInterval } = require('../core/utils.js'),
-    { zelda: zedIco } = require('../resx/emojis.json');
+const { randomIntFromInterval, getCurrentDatetime } = require('../core/utils.js'),
+    fs = require('node:fs'),
+    { parse } = require('json2csv'),
+    csvParse = require('fast-csv'),
+    { Weapons } = require('../core/classes/weapons.js');
+
+const weapons = new Weapons();
 
 module.exports = {
     data: {
@@ -12,27 +17,90 @@ module.exports = {
     async execute(message, client, language, args, initDateTime) {
         var roll = [],
             result = [],
-            item = [],
-            earn = [];
+            earn = [],
+            earnCsv = [],
+            dataUser = [];
 
-        for (let u = 0; u != 15; u++) {
-            let _rdm = randomIntFromInterval(0, zedIco.length);
-            item.push(client.emojis.cache.find(emoji => emoji.name === zedIco[_rdm]));
-        };
+        weapons.setPreRoll(client);
+
+        var sword = weapons.fiRoll,
+            claymore = weapons.seRoll,
+            shield = weapons.thRoll,
+            bow = weapons.foRoll,
+            axes = weapons.ffRoll;
 
         try {
-            for (let i = 0; i != 15; i++) {
-                roll.push(item[randomIntFromInterval(0, item.length - 1)]);
+            for (let i = 0; i < 15; i++) {
+                if (roll.length < 3) {
+                    roll.push(sword[randomIntFromInterval(0, (sword.length - 1))]);
+                } else if (3 <= roll.length && roll.length < 6) {
+                    roll.push(claymore[randomIntFromInterval(0, (claymore.length - 1))]);
+                } else if (6 <= roll.length && roll.length < 9) {
+                    roll.push(shield[randomIntFromInterval(0, (shield.length - 1))]);
+                } else if (9 <= roll.length && roll.length < 12) {
+                    roll.push(bow[randomIntFromInterval(0, (bow.length - 1))]);
+                } else if (12 <= roll.length && roll.length < 15) {
+                    roll.push(axes[randomIntFromInterval(0, (axes.length - 1))]);
+                };
+
                 if (roll.length % 3 == 0) {
                     let y = i - 1,
                         ii = i - 2;
                     if (roll[i].name == roll[y].name && roll[i].name == roll[ii].name) {
                         result.push('☑️');
-                        earn.push(roll[i].name.split('_').join(' '));
+                        earn.push(roll[i].name.slice(3).split('_').join(' '));
+                        earnCsv.push(roll[i].name);
                     } else { result.push('❌'); };
                 };
             };
-        } catch (err) { message.channel.send(`${message.author.username}: ${language.error}`); };
+        } catch (err) {
+            return message.channel
+                .send(`${language.errorRoll}`)
+                .then(() => {
+                    console.log(`[${getCurrentDatetime('comm')}] Error roll algo ${err}`);
+                });
+        };
+
+        if (earnCsv.length != 0) {
+            let filePath = `./data/inventory_user_roll.csv`;
+
+            for (let i = 0; i < earnCsv.length; i++) {
+                let data = {
+                    'id': Number(message.author.id),
+                    'user': String(message.author.username),
+                    'invent': String(earnCsv[i])
+                };
+
+                dataUser.push(data);
+            };
+
+            fs.exists(filePath, (e) => {
+                fs.createReadStream(filePath)
+                    .pipe(csvParse.parse({ delimiter: ',' }))
+                    .on('error', err => `[${getCurrentDatetime('comm')}] ${message.guild.name} / ${message.channel.name} # ${message.author.username}'s inventory error save ${err}`)
+                    .on('data', function (csvrow) {
+                        if (csvrow[0] == 'id') return;
+
+                        let data = {
+                            'id': Number(csvrow[0]),
+                            'user': String(csvrow[1]),
+                            'invent': String(csvrow[2])
+                        };
+
+                        dataUser.push(data);
+
+                        fs.writeFileSync(filePath, parse(dataUser), function (err) {
+                            if (err) {
+                                message.channel.send(`${language.errorRoll}`);
+                                console.log(`[${getCurrentDatetime('comm')}] ${message.guild.name} / ${message.channel.name} # ${message.author.username}'s inventory error save ${err}`);
+                                throw err;
+                            } else {
+                                console.log(`[${getCurrentDatetime('comm')}] ${message.guild.name} / ${message.channel.name} # ${message.author.username}'s inventory save`);
+                            };
+                        });
+                    });
+            });
+        };
 
         var fi_line = `${roll[0]} ${roll[1]} ${roll[2]} ${result[0]}`,
             se_line = `${roll[3]} ${roll[4]} ${roll[5]} ${result[1]}`,
@@ -49,10 +117,26 @@ module.exports = {
                 ff_line
             )
             .then(msg => {
-                if (earn.length != 0) { msg.channel.send(`${message.author.username}: ${language.winRoll} **${earn}** !`); }
-                else { msg.channel.send(`${message.author.username}: ${language.lossRoll}`); };
+                switch (earn.length) {
+                    case 1:
+                        msg.channel.send(`${message.author.username}: ${language.winRoll} **${earn}** !`);
+                        break;
+                    case 2:
+                        msg.channel.send(`${message.author.username}: ${language.winRoll} **${earn[0]}** !\n${language.winRoll2} **${earn[1]}** !!!`);
+                        break;
+                    case 3:
+                        msg.channel.send(`${message.author.username}: ${language.winRoll3} **${earn[0]}** + **${earn[1]}** + **${earn[2]}** WTFFF ...`);
+                        break;
+                    case 4:
+                        msg.channel.send(`${message.author.username}: ${language.winRoll41} **${earn[0]}**, **${earn[1]}**, **${earn[2]}**, **${earn[3]}** ${language.winRoll42} .....`);
+                        break;
+                    case 5:
+                        msg.channel.send(`${message.author.username}: **${earn[0]}**, **${earn[1]}**, **${earn[2]}**, **${earn[3]}**, **${earn[4]}** ${language.winRoll5}`);
+                        break;
+                    default:
+                        msg.channel.send(`${message.author.username}: ${language.lossRoll}`);
+                        break;
+                };
             });
-
-        await new Promise(resolve => setTimeout(resolve, 2 * 1000));
     }
 };
