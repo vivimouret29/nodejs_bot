43aca5ac4e09f5fa941432ec5b827a447884c079
Vivien Mouret <vivien.mouret@ynov.com>
2023-04-06 13:46:19 +0200

    ⚡️ overload console.log for exporting log

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 'use.strict'
 
 const { DaftBot } = require('./core/daftbot.js'),
+	fs = require('fs'),
+	util = require('util'),
 	daftBot = new DaftBot();
+
+var date = new Date();
+var logName =  `${date.getUTCDate() < 10 ? `0${date.getUTCDate()}` : date.getUTCDate()}-${date.getUTCMonth() < 10 ? `0${date.getUTCMonth()}` : date.getUTCMonth()}-${date.getUTCFullYear()}`;
+var logFile = fs.createWriteStream(`data/logs/log_daftbot_mobbot_${logName}.log`, { flags: 'a' }); // 'w' to truncate | 'a' to agglomerate
+var logStdout = process.stdout;
+
+console.log = function () {
+	logFile.write(util.format.apply(null, arguments) + '\n');
+	logStdout.write(util.format.apply(null, arguments) + '\n');
+}
+console.error = console.log;
 
 daftBot.onConnect();
