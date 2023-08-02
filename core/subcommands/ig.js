ffef0715a6f89285ae7fa9904ff6c1cd314d5d30
Vivien Mouret <vivien.mouret@ynov.com>
2023-08-02 12:03:24 +0200

    🚀 timer automatic message set

--- core/subcommands/ig.js
+++ core/subcommands/ig.js
@@ -6,7 +6,9 @@ module.exports = {
         description: 'a dynamic instant-gaming link'
     },
     async execute(client, channel, message, userstate, urI, timestamp, autoPost) {
-            client.say(channel, `daft est maintenant affilié à instant-gaming ! profites des promotions exceptionnelles de Terry en utilisant ce lien https://www.instant-gaming.com/?igr=daftmob daftmo1Gotanitem`)
-                .catch(e => console.log(e));
+        client.say(channel, `daft est maintenant affilié à instant-gaming, 
+        profites des promotions exceptionnelles de Terry en passant par ce lien https://www.instant-gaming.com/?igr=daftmob daftmo1Gotanitem 
+        et tentes de gagner des jeux gratuits en participant aux giveaway avec mon lien https://www.instant-gaming.com/fr/giveaway/instantgaming?igr=daftmob daftmo1Diosama`)
+            .catch(e => console.log(e));
     }
 };
--- core/subcommands/timer.js
+++ core/subcommands/timer.js
@@ -15,7 +15,8 @@ module.exports = {
                 'check quand je passe en live avec le twitter https://twitter.com/daftm0b Kappa',
                 `Terry t'offre une montagne de réduction sur Instant-Gaming en utilisant ce lien https://www.instant-gaming.com/?igr=daftmob daftmo1Gotanitem`,
                 `rejoins l\'ordre de daft en lâchant ton Prime ou en trouvant le chemin de la force https://www.twitch.tv/products/daftmob daftmo1Gotanitem`,
-                'avec le discord du daft https://discord.gg/ucwnMKKxZe et/ou avec son twitter https://twitter.com/daftm0b daftmo1Whaaa, tiens-toi au courant des prochains lives !'
+                'avec le discord du daft https://discord.gg/ucwnMKKxZe et/ou avec son twitter https://twitter.com/daftm0b daftmo1Whaaa, tiens-toi au courant des prochains lives !',
+                'tentes de gagner des jeux gratuits en participant aux giveaway avec mon lien https://www.instant-gaming.com/fr/giveaway/instantgaming?igr=daftmob daftmo1Diosama'
             ];
 
             client.say(channel, `daft est en live depuis ${timestamp}, ` + mtpPhrases[randomIntFromInterval(0, mtpPhrases.length - 1)])
