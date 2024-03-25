a6ca1c75ce3601d2886867f33ee40cf16c0e3f4b
Vivien Mouret <vivien.mouret@ynov.com>
2024-03-25 11:14:36 +0100

    🚀 update spreadsheets link

--- core/subcommands/mods.js
+++ core/subcommands/mods.js
@@ -8,7 +8,7 @@ module.exports = {
     async execute(client, channel, message, userstate, urI, timestamp, autoPost, sponso) {
         client
             .reply(channel,
-                'ptit lien des mods de stalker gamma : https://github.com/Grokitach/Stalker_GAMMA/blob/main/G.A.M.M.A/modpack_data/modlist.txt',
+                'ptite liste des mods de valheim : https://docs.google.com/spreadsheets/d/1xs6wocOW_jJBU4ork9Y5Z6XmIV03uiGTYBCbU6r_ODo/edit?usp=sharing daftmo1Whaaa',
                 userstate.id)
             .catch(e => console.log(e));
     }
