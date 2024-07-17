91b2691ff1d1a893e504902f7a7209de00b03f6d
Vivien Mouret <vivien.mouret@ynov.com>
2024-07-17 14:39:32 +0200

    🎨 updating main logs

--- main.js
+++ main.js
@@ -3,19 +3,25 @@
 const { DaftBot } = require('./core/daftbot.js'),
 	fs = require('fs'),
 	util = require('util'),
+	{ getCurrentDatetime } = require('./core/utils.js'),
 	moment = require('moment-timezone'),
 	utc2 = moment().tz('Europe/Paris'),
 	daftBot = new DaftBot();
 
-var logName = utc2.format('DD-MM-YYYY');
-var logFile = fs.createWriteStream(`data/logs/log_daftbot_mobbot_${logName}.log`, { flags: 'a' }); // 'w' to truncate | 'a' to agglomerate
-var logStdout = process.stdout;
+var logName = utc2.format('DD-MM-YYYY'),
+	logFile = fs.createWriteStream(`data/logs/log_daftbot_mobbot_${logName}.log`, { flags: 'a' }), // 'w' to truncate | 'a' to agglomerate
+	logStdout = process.stdout,
+	results = [];
+
+fs.readdirSync(__dirname).forEach(function (file) { results.push(file); });
+results = results.reverse();
+console.log(`[${getCurrentDatetime('comm')}] ` + util.inspect(results, false, null, true));
 
 console.log = function () {
 	logFile.write(util.format.apply(null, arguments) + '\n');
-
 	logStdout.write(util.format.apply(null, arguments) + '\n');
-}
+};
+
 console.error = console.log;
 
 daftBot.onConnect();
