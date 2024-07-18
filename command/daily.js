60549691426f17d6cd92379bcc695a6014575a81
Vivien Mouret <vivien.mouret@ynov.com>
2024-07-18 21:01:17 +0200

    🚀 daily command

---
+++ command/daily.js
@@ -0,0 +1,69 @@
+'use.strict'
+
+const { sendEmbed, getCurrentDatetime, randomIntFromInterval, getTimeRemaining } = require('../core/utils.js'),
+    csvParse = require('fast-csv'),
+    fs = require('node:fs'),
+    moment = require('moment-timezone'),
+    { parse } = require('json2csv');
+
+const filePathUser = `./data/user_roll.csv`;
+
+module.exports = {
+    data: {
+        name: 'daily',
+        description: 'a dynamic daily',
+        args: false
+    },
+    async execute(message, client, language, user, args, initDateTime) {
+        if (moment(user.dailyroll).tz('Europe/Paris').format() < moment().tz('Europe/Paris').format()) {
+            let updateDailyRoll = moment().tz('Europe/Paris').add(22, 'hours').format(),
+                winday = randomIntFromInterval(250, 1200),
+                text = `Tu viens de récupérer ta récompense quotidienne !
+<@${message.author.id}>, tu gagnes : **${winday}** ${client.emojis.cache.find(emoji => emoji.name === 'rubis')} !`;
+
+            await sendEmbed(message, text)
+                .catch(err => {
+                    message.reply({ 'content': language.error, 'ephemeral': false });
+                    console.log(`[${getCurrentDatetime('comm')}] Error sending message SEERROR ${err}`);
+                });
+
+            let usersProperty = [];
+            fs.createReadStream(filePathUser)
+                .pipe(csvParse.parse({ headers: true, delimiter: ',' }))
+                .on('data', row => {
+                    usersProperty.push({
+                        'id': Number(row.id),
+                        'username': String(row.username),
+                        'platform': String(row.platform),
+                        'pseudo': String(row.pseudo),
+                        'rubis': Number(row.rubis) + Number(winday),
+                        'canroll': row.canroll == 'true' ? true : false,
+                        'roll': Number(row.roll),
+                        'lastroll': String(moment(row.lastroll).tz('Europe/Paris').format()),
+                        'dailyroll': String(updateDailyRoll),
+                        'canwork': row.canwork == 'true' ? true : false,
+                        'lastwork': String(row.lastwork),
+                        'guildid': String(row.guildid)
+                    });
+                })
+                .on('end', () => {
+                    fs.writeFileSync(filePathUser, parse(usersProperty), function (err) {
+                        if (err) {
+                            message.channel.send(`${language.errorRoll}`);
+                            console.log(`[${getCurrentDatetime('comm')}] ${message.guild.name} / ${message.channel.name} # ${message.author.username}'s error save ${err}`);
+                            throw err;
+                        };
+                    });
+                });
+            console.log(`[${getCurrentDatetime('comm')}] ${message.guild.name} / ${message.channel.name} # ${message.author.username} got ${Number(winday)} for his daily reward`);
+        } else if (moment(user.dailyroll).tz('Europe/Paris').format() > moment().tz('Europe/Paris').format()) {
+            let duration = getTimeRemaining(user.dailyroll);
+            await sendEmbed(message, `Tu récupères ta prochaine récompense dans **${duration.hours} ${language.hours} ${duration.minutes} ${language.minutes} ${duration.seconds} ${language.seconds}**`)
+                .catch(err => {
+                    message.reply({ 'content': language.error, 'ephemeral': false });
+                    console.log(`[${getCurrentDatetime('comm')}] Error sending message SEERROR ${err}`);
+                });
+            console.log(`[${getCurrentDatetime('comm')}] ${message.guild.name} / ${message.channel.name} # ${message.author.username} already got his daily`);
+        };
+    }
+};
---
+++ slash/daily.js
@@ -0,0 +1,80 @@
+'use.strict'
+
+const { SlashCommandBuilder } = require('discord.js'),
+    { getCurrentDatetime, randomIntFromInterval, getTimeRemaining, randomColor } = require('../core/utils.js'),
+    csvParse = require('fast-csv'),
+    fs = require('node:fs'),
+    moment = require('moment-timezone'),
+    { parse } = require('json2csv');
+
+const filePathUser = `./data/user_roll.csv`;
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('daily')
+        .setDescription('Récupères ta récompense journalière !'),
+    async execute(message, client, language, user, args, initDateTime) {
+        if (moment(user.dailyroll).tz('Europe/Paris').format() < moment().tz('Europe/Paris').format()) {
+            let updateDailyRoll = moment().tz('Europe/Paris').add(22, 'hours').format(),
+                winday = randomIntFromInterval(250, 1200),
+                text = `Tu viens de récupérer ta récompense quotidienne !
+<@${message.user.id}>, tu gagnes : **${winday}** ${client.emojis.cache.find(emoji => emoji.name === 'rubis')} !`;
+
+            await message.reply({
+                'channel_id': message.channel.channel_id,
+                'embeds': [{
+                    'type': 'rich',
+                    'description': text,
+                    'color': randomColor()
+                }],
+                'fetchReply': false,
+                'ephemeral': false
+            })
+                .catch(err => { console.log(`[${getCurrentDatetime('comm')}] Error command daily send ${err}`); });
+
+            let usersProperty = [];
+            fs.createReadStream(filePathUser)
+                .pipe(csvParse.parse({ headers: true, delimiter: ',' }))
+                .on('data', row => {
+                    usersProperty.push({
+                        'id': Number(row.id),
+                        'username': String(row.username),
+                        'platform': String(row.platform),
+                        'pseudo': String(row.pseudo),
+                        'rubis': Number(row.rubis) + Number(winday),
+                        'canroll': row.canroll == 'true' ? true : false,
+                        'roll': Number(row.roll),
+                        'lastroll': String(moment(row.lastroll).tz('Europe/Paris').format()),
+                        'dailyroll': String(updateDailyRoll),
+                        'canwork': row.canwork == 'true' ? true : false,
+                        'lastwork': String(row.lastwork),
+                        'guildid': String(row.guildid)
+                    });
+                })
+                .on('end', () => {
+                    fs.writeFileSync(filePathUser, parse(usersProperty), function (err) {
+                        if (err) {
+                            message.channel.send(`${language.errorRoll}`);
+                            console.log(`[${getCurrentDatetime('comm')}] ${message.guild.name} / ${message.channel.name} # ${message.user.username}'s error save ${err}`);
+                            throw err;
+                        };
+                    });
+                });
+            console.log(`[${getCurrentDatetime('comm')}] ${message.guild.name} / ${message.channel.name} # ${message.user.username} got ${Number(winday)} for his daily reward`);
+        } else if (moment(user.dailyroll).tz('Europe/Paris').format() > moment().tz('Europe/Paris').format()) {
+            let duration = getTimeRemaining(user.dailyroll);
+            await message.reply({
+                'channel_id': message.channel.channel_id,
+                'embeds': [{
+                    'type': 'rich',
+                    'description': `Tu récupères ta prochaine récompense dans **${duration.hours} ${language.hours} ${duration.minutes} ${language.minutes} ${duration.seconds} ${language.seconds}**`,
+                    'color': randomColor()
+                }],
+                'fetchReply': false,
+                'ephemeral': false
+            })
+                .catch(err => { console.log(`[${getCurrentDatetime('comm')}] Error command daily send ${err}`); });
+            console.log(`[${getCurrentDatetime('comm')}] ${message.guild.name} / ${message.channel.name} # ${message.user.username} already got his daily`);
+        };
+    }
+};
